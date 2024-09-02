4c928196fa557e5b49d7d3c901fb088ca583fb9c
Матвей <matvei@MacBook-Pro-Matvej.local>
2024-09-02 21:13:55 +0300

    New commit

--- app/src/test/java/hexlet/code/FirstTest.java
+++ app/src/test/java/hexlet/code/FirstTest.java
@@ -4,13 +4,12 @@ import hexlet.code.Main;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 
 public class FirstTest {
 
     @Test
     public void mainTest() {
-        var a = Main.plus(1,1);
+        var a = Main.plus(1, 1);
         assertEquals(2, 2);
     }
 }
