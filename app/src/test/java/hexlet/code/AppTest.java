b3b52b767948e5238fca591eb8295bc57d646156
Матвей <matvei@MacBook-Pro-Matvej.local>
2024-09-30 15:10:23 +0300

    New commit

--- app/src/test/java/hexlet/code/AppTest.java
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -97,7 +97,7 @@ public class AppTest {
         JavalinTest.test(app, (server, client) -> {
             var response = client.get("/");
             assertThat(response.code()).isEqualTo(200);
-            assertThat(response.body().string()).contains("Анализатор сайтов");
+            assertThat(response.body().string()).contains("Анализатор страниц");
         });
     }
 
